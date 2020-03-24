5c24b119d16fcee55bf2ea6070d2552d506912bf
Guy rahamim <guy.danino5@gmail.com>
2020-03-24 19:14:09 +0200

    Initial Commit
    A small Unity project called Number Wizard, created for the porpuse of learning C#

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+
+{		int guess = 500;
+		int max = 1000;
+		int min = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+		Debug.Log("Welcome to Number Wizard");
+		Debug.Log("Please choose a number from the following range:");
+		Debug.Log("Lowest: " + min);
+		Debug.Log("Highest: "+max);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Debug.Log("Up arrow is pressed");
+			max = guess;
+		}
+
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Debug.Log("Down arrow is pressed");
+			min = guess;
+		}
+
+		else if (Input.GetKeyDown(KeyCode.Return))
+		{
+			Debug.Log("Enter is pressed");
+		}
+
+	}
+}
